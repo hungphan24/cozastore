c718a1c6f03275e4e98c1a35ec2586e4e001f201
hung.phan <duchungeden@gmail.com>
2023-08-05 16:10:03 +0700

    logging

--- src/main/java/com/hung/cozastore/controller/productController.java
+++ src/main/java/com/hung/cozastore/controller/productController.java
@@ -1,7 +1,10 @@
 package com.hung.cozastore.controller;
 
+import com.google.gson.Gson;
 import com.hung.cozastore.payload.response.BaseResponse;
 import com.hung.cozastore.service.interf.IProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +19,17 @@ public class productController {
     @Autowired
     private IProductService iProductService;
 
+    private Logger logger = LoggerFactory.getLogger(productController.class);
+    private Gson gson = new Gson();
+
     @GetMapping("/category/{id}")
     public ResponseEntity<?> getProductByCategory(@PathVariable int id) {
+        logger.info("tham so " + id);
         BaseResponse response = new BaseResponse();
         response.setStatusCode(200);
         response.setData(iProductService.getProductByCategory(id));
+
+        logger.info(gson.toJson(response));
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
